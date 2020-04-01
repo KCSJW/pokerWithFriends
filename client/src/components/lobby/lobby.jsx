bb95e46412f6fff3093dff0d46965575526a3c79
KJW <johnson9710@gmail.com>
2020-03-31 21:26:56 -0700

    added code for lobby

--- client/src/components/lobby/lobby.jsx
+++ client/src/components/lobby/lobby.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
-const Lobby = () => {
-  return <div>Lobby!</div>;
-};
+class Lobby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const socket = MySocket.getSocket(this.props.currentUser.id);
+
+    socket.off("lobby");
+    socket.off("removeRoom");
+    socket.off("updateRoom");
+
+    socket.on("lobby", payload => {
+      this.props.receiveRooms(payload.rooms);
+      if (payload.roomId) {
+        const user = this.props.currentUser;
+        user.roomId = payload.roomId;
+        this.props.joinRoom(user);
+      }
+    });
+
+    socket.on("removeRoom", payload => {
+      this.props.removeRoom(payload.id);
+    });
+
+    socket.on("updateRoom", data => {
+      this.props.receiveRoom(data);
+    });
+
+    socket.emit("getRooms");
+  }
+
+  render() {
+    return (
+      <div className="lobby-page">
+        <div className="lobby">
+          <GameRooms
+            className="game-rooms"
+            currentUser={this.props.currentUser}
+            rooms={this.props.rooms}
+            createRoom={this.props.createRoom}
+            joinRoom={this.props.joinRoom}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Lobby;
